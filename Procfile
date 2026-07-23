2c44fadd62da4f9794344f26b567dc1bff2f702f
sezenkorkmaz <sezenkkorkmazz@gmail.com>
2026-07-23 09:18:12 +0300

    deploy hazırlığı: gunicorn, whitenoise, Procfile eklendi

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn izin_takip.wsgi
--- izin_takip/settings.py
+++ izin_takip/settings.py
@@ -1,12 +1,13 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure--(_3d&9#%o3e=os^wyk_6l=77%^@su$i7p7+dob546$$m1omsu'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--(_3d&9#%o3e=os^wyk_6l=77%^@su$i7p7+dob546$$m1omsu')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -20,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +72,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/giris/'
 LOGIN_REDIRECT_URL = '/panel/'
